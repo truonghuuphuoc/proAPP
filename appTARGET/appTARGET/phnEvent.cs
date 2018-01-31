b549fb084951c2c7227b82b126d07abbb6616070
Henry Truong <henry.truong@styl.solutions>
2018-01-31 09:05:59 +0700

    - Add new file phnEvent.cs

---
+++ appTARGET/appTARGET/phnEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appTARGET
+{
+    class phnEvent
+    {
+        public byte Be { get; set; }
+        public byte Bia { get; set; }
+
+        public byte Diem { get; set; }
+
+        public phnEvent(byte _be, byte _bia, byte _diem)
+        {
+            this.Be = _be;
+            this.Bia = _bia;
+            this.Diem = _diem;
+        }
+
+    }
+}
